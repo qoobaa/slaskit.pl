706db0c1aac20a628076a4e02c82aaef219dccf3
Kuba Kuźma <kuba@jah.pl>
2014-11-18 21:18:22 +0100

    added simple angular controller

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
-angular.module("slaskIT", [])
+var slaskIT = angular.module("slaskIT", []);
 
-.controller("EventsController", function ($scope) {
+slaskIT.constant("PIPE_URL", "https://pipes.yahoo.com/pipes/pipe.run?_id=328d9ea8b0d9dba60535103a6c3a532d&_render=json&days=7");
 
+slaskIT.controller("EventsController", function ($scope, $http, PIPE_URL) {
+    $scope.events = [];
+
+    $http.get(PIPE_URL).then(function (response) {
+        $scope.loaded = true;
+        $scope.events = response.data.value.items;
+    });
 });
