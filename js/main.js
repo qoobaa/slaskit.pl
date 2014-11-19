23b955f517f9a47960f5292a3354f2c49a7e0cd0
Kuba Kuźma <kuba@jah.pl>
2014-11-19 19:50:41 +0100

    added fetch button

--- js/main.js
+++ js/main.js
@@ -1,12 +1,15 @@
 var slaskIT = angular.module("slaskIT", []);
 
-slaskIT.constant("PIPE_URL", "https://pipes.yahoo.com/pipes/pipe.run?_id=328d9ea8b0d9dba60535103a6c3a532d&_render=json&days=7");
+slaskIT.constant("PIPE_URL", "https://pipes.yahoo.com/pipes/pipe.run?_id=328d9ea8b0d9dba60535103a6c3a532d&_render=json");
 
 slaskIT.controller("EventsController", function ($scope, $http, PIPE_URL) {
-    $scope.events = [];
+    $scope.fetch = function (days) {
+        $http.get(PIPE_URL + "&days=" + days).then(function (response) {
+            $scope.days = days;
+            $scope.loaded = true;
+            $scope.events = response.data.value.items;
+        });
+    };
 
-    $http.get(PIPE_URL).then(function (response) {
-        $scope.loaded = true;
-        $scope.events = response.data.value.items;
-    });
+    $scope.fetch(7);
 });
