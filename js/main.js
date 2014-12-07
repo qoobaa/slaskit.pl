ebb1b697e76155173511dcd6590dd5f1506e0cbb
Kuba Kuźma <kuba@jah.pl>
2014-12-07 10:54:08 +0100

    removed button

--- js/main.js
+++ js/main.js
@@ -1,15 +1,14 @@
 var slaskIT = angular.module("slaskIT", []);
 
-slaskIT.constant("PIPE_URL", "https://pipes.yahoo.com/pipes/pipe.run?_id=328d9ea8b0d9dba60535103a6c3a532d&_render=json");
+slaskIT.constant("PIPE_URL", "https://pipes.yahoo.com/pipes/pipe.run?_id=328d9ea8b0d9dba60535103a6c3a532d&_render=json&days=365");
 
 slaskIT.controller("EventsController", function ($scope, $http, PIPE_URL) {
-    $scope.fetch = function (days) {
-        $http.get(PIPE_URL + "&days=" + days).then(function (response) {
-            $scope.days = days;
+    $scope.fetch = function () {
+        $http.get(PIPE_URL).then(function (response) {
             $scope.loaded = true;
             $scope.events = response.data.value.items;
         });
     };
 
-    $scope.fetch(7);
+    $scope.fetch();
 });
