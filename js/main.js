f7c54ac4c416234579bf98ef2462060d944ad1ca
Kuba Kuźma <kuba@jah.pl>
2014-12-12 21:50:13 +0100

    imported Maciek's styles

--- js/main.js
+++ js/main.js
@@ -3,10 +3,39 @@ var slaskIT = angular.module("slaskIT", []);
 slaskIT.constant("PIPE_URL", "https://pipes.yahoo.com/pipes/pipe.run?_id=328d9ea8b0d9dba60535103a6c3a532d&_render=json&days=365");
 
 slaskIT.controller("EventsController", function ($scope, $http, PIPE_URL) {
+    $scope.dayMatcher = function (day) {
+        return function (event) {
+            var date = event["y:published"];
+
+            return date.year === day.year
+                && date.month === day.month
+                && date.day === day.day;
+        };
+    };
+
     $scope.fetch = function () {
         $http.get(PIPE_URL).then(function (response) {
             $scope.loaded = true;
+            $scope.days = [];
             $scope.events = response.data.value.items;
+
+            $scope.events.forEach(function (event) {
+                var lastDay = $scope.days[$scope.days.length - 1],
+                    date = event["y:published"];
+
+                if (!lastDay
+                    || lastDay.year  !== date.year
+                    || lastDay.month !== date.month
+                    || lastDay.day   !== date.day) {
+
+                    $scope.days.push({
+                        year: date.year,
+                        month: date.month,
+                        day: date.day,
+                        event: event
+                    });
+                }
+            });
         });
     };
 
